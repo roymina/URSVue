e754d6550589bfab5c9bbf526e0fb2940e857bad
Roy <jnmcluo@163.com>
2023-09-05 23:27:38 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div ref="receiverRef"></div>
+</template>
+
+<script setup>
+import {getServerConfig, getRTCConfiguration} from "@/unitylib/config";
+import {createDisplayStringArray} from "@/unitylib/stats.js";
+import {VideoPlayer} from "@/unitylib/videoplayer.js";
+
+import {onMounted, ref} from "vue";
+const receiverRef = ref(null);
+onMounted(async () => {
+  await setup();
+});
+const setup = async () => {
+  const res = await getServerConfig();
+  useWebSocket = res.useWebSocket;
+};
+</script>
